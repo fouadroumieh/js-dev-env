742b3936e2b48742eb5ee70f5b0e61c268b692fb
fouadroumieh <fouadromieh@gmail.com>
2017-05-08 13:18:49 +0200

    mocking and prod changes

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -1,18 +1,13 @@
 import express from 'express';
 import path from 'path';
 import opn from 'opn';
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
+import compression from 'compression';
 
 const port = 3000;
 const app = express();
-const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
-
+app.use(express.static('dist'));
+app.use(compression());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.htm'));
